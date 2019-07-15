980a24f3dd3789e366726ddfe9e256e4fc582da1
Ashanka Saha <ashanka@Ashankas-MacBook-Air.local>
2019-07-15 14:33:52 +0530

    solved binary string

---
+++ Strings/binary_string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char *str;
+    int t, n;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        int cnt = 0;
+        str = new char[n];
+        cin >> str;
+        for(int i=0; i<n; i++)
+        {
+            if(str[i] == '1');
+                cnt++;
+        }
+
+        cout << cnt * (cnt-1) / 2 << endl;
+    }
+
+    return (0);
+}
